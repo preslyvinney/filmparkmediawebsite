4caa75f3a880c0428fb1b0ed414f27889ded5905
Presly Vinney <PreslyVinney@gmail.com>
2026-04-16 22:43:12 -0500

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://filmparkmedia.com"),
